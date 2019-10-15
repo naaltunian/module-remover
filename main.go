51a1c78bee9bdb2e5368aa0696c0760512eb9d25
Nick <naaltunian@icloud.com>
2019-10-15 08:17:31 -0400

    Initial Commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	var dirName = "node_modules"
+	var err error
+
+	path := getPath()
+
+	err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return filepath.SkipDir
+		}
+		if info.IsDir() && info.Name() == dirName {
+			fmt.Printf("Deleting: %+v \n", info.Name())
+			os.RemoveAll(path)
+		}
+		fmt.Printf("Visited file or dir: %q\n", path)
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getPath() string {
+	var path string
+	// get home directory
+	homeDir, err := os.UserHomeDir()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Provide a path to delete all node_modules directories:")
+	fmt.Scanln(&path)
+	// prevent deleting all node_modules on home path
+	if path == homeDir {
+		fmt.Println("Cannot use home directory for safety purposes. Enter a different path")
+		getPath()
+	}
+	return path
+}
